e07518b9960305d1f5d200bb205e41f153aaa836
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-09-14 18:16:25 +0200

    Transaction Table creation

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS momo_sms;
 
 USE momo_sms;
 
-CREATE TABLE users (
+CREATE TABLE Users (
     userId INT PRIMARY KEY AUTO_INCREMENT COMMENT 'Unique identifier for each user',
     phoneNumber VARCHAR(15) NOT NULL UNIQUE COMMENT 'Mobile money subscriber MSISDN',
     firstName VARCHAR(50) NOT NULL COMMENT 'User first name',
@@ -13,3 +13,16 @@ CREATE TABLE users (
     momoBalance DECIMAL(12, 2) DEFAULT 0.00 CHECK (momoBalance >= 0) COMMENT 'Current mobile money balance'
 ) COMMENT = 'Stores registered MoMo system users';
 
+CREATE TABLE Transactions (
+    transactionId INT PRIMARY KEY,
+    timestamp DATETIME,
+    status VARCHAR(20),
+    senderId INT,
+    receiverId INT,
+    currentAmount DECIMAL(12,2),
+    transactionFee DECIMAL(10,2),
+    governmentTax DECIMAL(10,2),
+    senderBalanceAfter DECIMAL(12,2),
+    receiverBalanceAfter DECIMAL(12,2),
+    referenceText VARCHAR(255)
+);
