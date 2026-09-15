425d43334f847cb9d6efd02fb9d0163b1fe0bdd5
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-09-15 22:41:45 +0200

    CHECK Constraints testing

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -111,3 +111,6 @@ VALUES (NULL, 'Test', '0788999111');
 
 INSERT INTO users (firstName, lastName, phoneNumber)
 VALUES ('Fake', 'User', '0788111222');
+
+INSERT INTO transactions (senderId, receiverId, transactionTimestamp, status, currentAmount)
+VALUES (1, 2, NOW(), 'SUCCESS', -500.00);
